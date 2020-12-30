511bd7660c4d775adad084d9d35194c251312160
Crowdbotics <team@crowdbotics.com>
2020-12-30 13:46:10 +0000

    Update components

--- src/features/BlankScreen116585/screens/blank.js
+++ src/features/BlankScreen116585/screens/blank.js
@@ -29,7 +29,14 @@ export default class Blank extends React.Component {
   render = () => (
     <View>
       <View>
-        <View style={styles.View_6}>
+        <View style={styles.View_6} />
+        <View style={styles.View_7}>
+          <Image
+            source={{
+              uri:
+                "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/2_rM446w7.jpg"
+            }}
+          />
           <Switch
             trackColor={{ false: "#C0CCDA", true: "#409EFF" }}
             style={styles.Switch_17}
@@ -37,14 +44,6 @@ export default class Blank extends React.Component {
             onValueChange={nextChecked =>
               this.setState({ Switch_17: nextChecked })
             }
-          />
-        </View>
-        <View style={styles.View_7}>
-          <Image
-            source={{
-              uri:
-                "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/2_rM446w7.jpg"
-            }}
           />
         </View>
       </View>
@@ -56,7 +55,7 @@ const styles = StyleSheet.create({
   View_1: {},
   View_5: {},
   View_6: { width: 300 },
-  Switch_17: { alignSelf: "flex-start" },
   View_7: { width: 300, height: 200 },
-  Image_15: {}
+  Image_15: {},
+  Switch_17: { alignSelf: "flex-start" }
 })
