7945a50e6f0acbcb84968de2dd0cc7b9556eac95
Crowdbotics <team@crowdbotics.com>
2020-12-30 13:17:02 +0000

    Update components

--- src/features/BlankScreen116585/screens/blank.js
+++ src/features/BlankScreen116585/screens/blank.js
@@ -28,27 +28,41 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View>
+      <Text>Sample text content</Text>
       <Image
         source={{
           uri:
             "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/2_rM446w7.jpg"
         }}
       />
-      <Text>Sample text content</Text>
       <TextInput
         placeholder="Number Input Placeholder"
-        editable={true}
         keyboardType="numeric"
         value={this.state.TextInput_4}
         onChangeText={nextValue => this.setState({ TextInput_4: nextValue })}
       />
+      <View>
+        <View style={styles.View_6} />
+        <View style={styles.View_7}>
+          <Image
+            source={{
+              uri:
+                "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/2_rM446w7.jpg"
+            }}
+          />
+        </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   View_1: {},
-  Image_2: {},
   Text_3: {},
-  TextInput_4: {}
+  Image_2: {},
+  TextInput_4: {},
+  View_5: {},
+  View_6: { width: 300 },
+  View_7: { width: 300, height: 200 },
+  Image_15: {}
 })
